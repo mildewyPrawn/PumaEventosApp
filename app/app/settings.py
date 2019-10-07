8bba2ee7af1d51ee1b516c0b171475b25fb72488
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-10-07 15:42:25 -0500

    db user, pass, name

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'puma_app_db',
+        'USER': 'puma_app_admin', 
+        'PASSWORD': os.environ.get("DB_KEY"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
