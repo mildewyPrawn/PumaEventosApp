b244f9a94dca61685369abba990c01ea7eafd010
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-10-09 18:26:54 -0500

    models

--- app/User/models.py
+++ app/User/models.py
@@ -1,3 +1,16 @@
+import django
 from django.db import models
-
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=42)
+    apellido = models.CharField(max_length=255)
+    email = models.EmailField(max_length=75)
+    #contraseña
+    #foto
+
+class EntidadAcademica(models.Model):
+    nombre = models.CharField(max_length=255)
