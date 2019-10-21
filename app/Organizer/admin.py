f6dca617a626857261683220c5da967a717f6220
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2019-10-21 16:21:25 -0500

    commit vacio

--- app/Organizer/admin.py
+++ app/Organizer/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Evento
+from .models import Etiqueta
 # Register your models here.
 
 admin.site.register(Evento)
+admin.site.register(Etiqueta)
