100b3662835da197eeca6e86b2a8befc353b9235
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2019-10-21 13:46:00 -0500

    try

--- app/Organizer/models.py
+++ app/Organizer/models.py
@@ -2,8 +2,13 @@ import django
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
-from User.models import Usuario
+from User.models import User
+from composite_field import CompositeField
 # Create your models here.
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=255)
+
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=255)
@@ -16,28 +21,26 @@ class Evento(models.Model):
     horario_fin = models.TimeField()
     capacidad = models.IntegerField()
     etiqueta = models.ManyToManyField(Etiqueta)
-    organizador = models.OneToOneField(Usuario, on_delete=models.CASCADE, parent_link=True)
+    organizador = models.OneToOneField(User, on_delete=models.CASCADE, parent_link=True)
     #periodicidad =
 
-class Etiqueta(models.Model):
-    nombre = models.CharField(max_length=255)
 
 
 class Invitacion(models.Model):
     evento_id = models.ForeignKey(Evento, on_delete=models.CASCADE)
-    usuario_id = models.ForeignKey(Usuario)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     activa = models.BooleanField()
     asistencia_activa = models.BooleanField()
-    id = models.CompositeField(evento_id,usuario_id, primary_key=True)
+    id = CompositeField(evento_id,User_id, primary_key=True)
     #codigo_qr ......
 
 class Staff(models.Model):
-    usuario_id = models.ManyToManyField(Usuario)
+    user_id = models.ManyToManyField(User)
     evento_id = models.ManyToManyField(Evento)
-    staff_id = models.OneToOneField(Usuario, on_delete=models.CASCADE,
+    staff_id = models.OneToOneField(User, on_delete=models.CASCADE,
                                     parent_link=True)
 
 class ValidaInvitacion(models.Model):
-    usuario_id = models.ForeignKey(Staff)
+    user_id = models.ForeignKey(Staff)
     invitacion_id = models.ForeignKey(Invitacion)
-    id = models.CompositeField(usuario_id,invitacion_id, primary_key=True)    
+    id = models.CompositeField(User_id,invitacion_id, primary_key=True)    
