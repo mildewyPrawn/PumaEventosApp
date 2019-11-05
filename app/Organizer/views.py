5c4cba444e5650874b3fd18154336960e43a44fb
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2019-11-05 16:40:48 -0600

    Started work with Crud

--- app/Organizer/views.py
+++ app/Organizer/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Evento
+from .forms import eventsForm
 
 # Create your views here.
+
+
+def listMyEvents(request):
+	events=Evento.objects.all()
+	return render(request,'myEvents.html',{'events':events})
+
+
+
+def createEvent(request):
+	form = EventoForm(request.Post or None)
+
+	if form.is_valid():
+		form.save()
+		return redirect('listMyEvents')
+
+	return render(request,'eventsForm.html',{'form':form})
+
+
+def updateEvent(request):
+	event = Evento.objects.get(id=id)
+	form = EventoForm(request.Post or None, instance=event)
+
+	if form.is_valid():
+		form.save()
+		return redirect('listMyEvents')
+
+	return render(request,'eventsForm.html',{'form':form})
+
+
+def deleteEvent(request):
+
+	pass
+
