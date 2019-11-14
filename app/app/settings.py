7eb0c6db21724a47ed7a656a564a7af2c4a1291f
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-11-13 18:03:11 -0600

    lineas cortas

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,6 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# Email sending
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAI_L')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_KEY')
+EMAIL_PORT = 587
+
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
