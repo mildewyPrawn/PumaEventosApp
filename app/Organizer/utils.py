1ea16406a11043ec430180d653ac6107959ad54c
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-11-28 19:53:02 -0600

    send_email func

---
+++ app/Organizer/utils.py
@@ -0,0 +1,11 @@
+"""Utils for send mail"""
+
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_email(to_email, subject, content):
+    """
+    Sends email
+    """
+    email_from = settings.EMAIL_HOST_USER
+    send_mail(subject, content, email_from, to_email)
